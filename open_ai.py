6e7a4ac864073b3bd4ec31707992881dc90e33d1
asif laldin <laldin.asif@gmail.com>
2023-01-28 10:48:47 +0000

    ading files

---
+++ open_ai.py
@@ -0,0 +1,29 @@
+import os
+import openai
+import json 
+os.environ['SECRET_KEY'] = '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'
+openai.api_key = os.getenv("SECRET_KEY")
+
+prompt = 'In python how do you write the fizzbuzz technique without using a for loop'
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt= prompt,
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+
+openai_object_json = json.dumps(response,indent=4)
+openai_object_data = json.loads(openai_object_json)
+formatted_text = openai_object_data['choices'][0]['text']
+
+print(f'\nThe prompt was:\n {prompt}\n')
+print("--" * 25)
+print(f'\nThe formatted response is :\n {formatted_text}\n')
+
+
+
+
